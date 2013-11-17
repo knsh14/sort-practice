7e310df892cc9f788a9d52d9f119cf2d6a3eb2c3
kenshi kamata <kenshi.kamata@gmail.com>
2013-11-17 18:23:17 +0900

    finished bubble sort

--- sort.rb
+++ sort.rb
@@ -8,6 +8,13 @@ class SortLibrary
     end
 
     def bubble_sort
+        0.upto(@list.size-1) do |i|
+            (@list.size-1).downto(i) do |j|
+                if @list[j] < @list[j-1] then
+                    @list[j], @list[j-1] = @list[j-1], @list[j]
+                end
+            end
+        end
         @list
     end
 end
