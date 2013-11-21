39f0dab46df004e093813764e6e364bea4702319
kenshi kamata <kenshi.kamata@gmail.com>
2013-11-22 00:49:55 +0900

    test unit generate array with list comprehensions.

--- test_sort.rb
+++ test_sort.rb
@@ -3,7 +3,7 @@ require './sort.rb'
 
 class TestCase_Sort < Test::Unit::TestCase
     def setup
-        @nbr_arry = [1,2,3,4,5,6,7,8,9,10]
+        @nbr_arry = [*1..20]
         @rand_nbr_arry = @nbr_arry.sort_by{rand}
         @sort_obj = SortLibrary.new(@rand_nbr_arry)
     end
